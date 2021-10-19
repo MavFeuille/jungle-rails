7bce96f6115a154a2d4b3853c0d1273b24dc5639
MavFeuille <mavis.feuille@gmail.com>
2021-10-18 23:19:44 -0400

    Add test for email, first name, last name should be required

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe User, type: :model do
       expect(@user).to_not be_valid
     end
 
-    it "email must be unique" do
+    it "email must be unique - exact same email" do
       user1 = User.create(
         first_name: 'user1 first',
         last_name: 'user1 last',
@@ -29,13 +29,58 @@ RSpec.describe User, type: :model do
         password_confirmation: '123'
       )
       expect(user2).to_not be_valid
-
     end
 
+    it "email must be unique - not case sensitive" do
+      user1 = User.create(
+        first_name: 'user1 first',
+        last_name: 'user1 last',
+        email: 'user1@user1.com',
+        password: '123',
+        password_confirmation: '123'
+      )
+      user2 = User.create(
+        first_name: 'user2 first',
+        last_name: 'user2 last',
+        email: 'user1@user1.COM',
+        password: '123',
+        password_confirmation: '123'
+      )
+      expect(user2).to_not be_valid
+    end
 
+    it "is not valid if email is nil" do
+      @user = User.create(
+        first_name: 'test first name',
+        last_name: 'test last name',
+        # email: nil,
+        password: 'testpw',
+        password_confirmation: 'teStpw'
+      )
+      expect(@user).to_not be_valid
+    end
+    
+    it "is not valid if first name is nil" do
+      @user = User.create(
+        first_name: nil,
+        last_name: 'test last name',
+        email: '1@1.com',
+        password: 'testpw',
+        password_confirmation: 'teStpw'
+      )
+      expect(@user).to_not be_valid
+    end
 
-
-
+    it "is not valid if last name is nil" do
+      @user = User.create(
+        first_name: 'test first name',
+        last_name: nil,
+        email: '1@1.com',
+        password: 'testpw',
+        password_confirmation: 'teStpw'
+      )
+      expect(@user).to_not be_valid
+    end
 
   end
 end
