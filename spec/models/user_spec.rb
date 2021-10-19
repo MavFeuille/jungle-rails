737293314d8f24b6fc0bdd6fba3ebb02df704af8
MavFeuille <mavis.feuille@gmail.com>
2021-10-18 21:21:29 -0400

    Create validation for user password set up

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it "is not valid if password and password_confirmation does not match" do
+      @user = User.create(
+        first_name: 'test first name',
+        last_name: 'test last name',
+        email: '1@1.com',
+        password: 'testpw',
+        password_confirmation: 'teStpw'
+      )
+      expect(@user).to_not be_valid
+    end
+
+
+
+
+
+
+  end
 end
